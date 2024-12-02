c5ace8b78f944b54dcf587caf59f591b2ed382d2
Emilia Mailian <emiliamailian@gmail.com>
2024-12-02 16:17:05 +0100

    hittills fungerar allt förutom delete funktionen

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const noteRoutes = require('./routes/noteRoutes');
+const noteRoutes = require('./routes/noteRoutes');  
+const connectDB = require('./config/db')
 
 dotenv.config();
 
 const app = express();
-connectDB();
 
-// Middleware
 app.use(express.json());
 
-// Routes
-app.use('/api/notes', noteRoutes);
 
-// Start server
+connectDB();
+
+app.use('/api/notes', noteRoutes); 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+
+
